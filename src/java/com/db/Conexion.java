8941f810b92dfa46c217dc74ee4edafaad687ebd
Alexis Guerra <>
2017-10-20 11:11:25 -0600

    Conexion y Bean usuario.

---
+++ src/java/com/db/Conexion.java
@@ -0,0 +1,37 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Conexion {
+    private Connection conexion;
+    private Statement stm;
+    private static Conexion instancia;
+
+    public static synchronized Conexion getInstancia(){
+        return (instancia == null)?new Conexion():instancia;		
+    }
+    
+    public Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conexion=DriverManager.getConnection("jdbc:google:mysql://proyecto-java:dbproyecto-183021:us-central1:proyecto-java/proyecto", "root", "root");
+            stm=conexion.createStatement();
+        }catch(InstantiationException e){
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(IllegalAccessException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/java/com/model/Usuario.java
@@ -0,0 +1,145 @@
+package com.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Usuario {
+    private int id_usuario;
+    private String nombres;
+    private String apellidos;
+    private int sexo;
+    private String email;
+    private java.sql.Date fecha_nacimiento; 
+    private java.sql.Date fecha_creacion; 
+    private int id_rol;
+    private String foto;
+    private int id_pàis;
+    private String telefono;
+    private String clave_acceso;
+    private int estado;
+
+    public Usuario(int id_usuario, String nombres, String apellidos, int sexo, String email, Date fecha_nacimiento, Date fecha_creacion, int id_rol, String foto, int id_pàis, String telefono, String clave_acceso, int estado) {
+        this.id_usuario = id_usuario;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.sexo = sexo;
+        this.email = email;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.fecha_creacion = fecha_creacion;
+        this.id_rol = id_rol;
+        this.foto = foto;
+        this.id_pàis = id_pàis;
+        this.telefono = telefono;
+        this.clave_acceso = clave_acceso;
+        this.estado = estado;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public Date getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(Date fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public int getId_pàis() {
+        return id_pàis;
+    }
+
+    public void setId_pàis(int id_pàis) {
+        this.id_pàis = id_pàis;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getClave_acceso() {
+        return clave_acceso;
+    }
+
+    public void setClave_acceso(String clave_acceso) {
+        this.clave_acceso = clave_acceso;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    
+}
